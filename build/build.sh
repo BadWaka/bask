20c33a21eb3b81c2fdb03d316f96751a8c9e0505
wangkai37 <wangkai37@baidu.com>
2021-11-30 17:55:50 +0800

    deploy

--- build/build.sh
+++ build/build.sh
@@ -1,4 +1,10 @@
 cd fe
 npm run build
 rm -rf ../server/static/dist
-mv dist ../server/static/
+rm -rf ../server/static/css
+rm -rf ../server/static/fonts
+rm -rf ../server/static/js
+rm -rf ../server/static/favicon.ico
+rm -rf ../server/static/index.html
+mv dist/* ../server/static/
+npm run server:restart
