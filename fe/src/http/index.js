ca23a59160cd0227a628c9bd3ee62a34d892c193
wangkai37 <wangkai37@baidu.com>
2021-12-21 11:51:55 +0800

    https

--- fe/src/http/index.js
+++ fe/src/http/index.js
@@ -1,8 +1,8 @@
 const axios = require('axios');
 
 // const config = require('./config.json');
-// const host = `https://www.baskclub.com`;
-const host = `http://localhost:8848`;
+const host = `https://www.baskclub.com`;
+// const host = `http://localhost:8848`;
 
 async function getPeople() {
     const res = await axios({
